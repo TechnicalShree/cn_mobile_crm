b2761ba33370be8e32a29f91aafaf32219d9d651
TechnicalShree <krushnasr96@gmail.com>
2025-02-26 14:32:18 +0530

    Add Daily Visit Schedule DocType and related files

---
+++ cn_mobile_crm/cn_mobile_crm/doctype/daily_visit_schedule/test_daily_visit_schedule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Chinmay and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDailyVisitSchedule(FrappeTestCase):
+	pass
