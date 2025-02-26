66c38abcd60b5803fbe564ed7bc2de1727ad56b9
TechnicalShree <krushnasr96@gmail.com>
2025-02-26 14:59:09 +0530

    Add Budget Range DocType and related files

---
+++ cn_mobile_crm/cn_mobile_crm/doctype/budget_range/budget_range.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Chinmay and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Budget Range", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ cn_mobile_crm/cn_mobile_crm/doctype/budget_range/test_budget_range.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Chinmay and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBudgetRange(FrappeTestCase):
+	pass
